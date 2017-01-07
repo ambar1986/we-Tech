9ffbece3cdcb0ed016fe90c91b2739a6b2bd2219
sfu <sfu.concepts@gmail.com>
2017-01-07 18:17:27 -0500

    laid out app.js and home.jsx

--- frontend/components/app.js
+++ frontend/components/app.js
@@ -1,12 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Router, Route, IndexRoute, Link, browserHistory} from 'react-router';
+import Home from './home';
 
 var App = React.createClass({
-	render: function(){
+	render(){
 		return(
-			<div>HELLO</div>
+			<div>
+				<div className="logo-class">
+					<img className="logo" src={require('../images/weTech-logo.png')} />
+				</div>
+
+				<nav className="navbar navbar-inverse">
+		          <div className="container-fluid">
+		            <div className="nav navbar-nav">
+		              <Link to='/'>Home</Link> 
+		              <Link to='#'>About</Link>
+		              <Link to='#'>FAQ</Link>
+		              <Link to='#'>Sign Up</Link>
+	            	</div>
+	          	  </div>
+	        	</nav>
+	        	{this.props.children}			
+        	</div>
 		)
 	}
 })
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+	<Router history={browserHistory}>
+		<Route path='/' component={App}>
+		<IndexRoute component={Home} />
+
+		</Route>
+	</Router>, 
+	document.getElementById('root'))
---
+++ frontend/components/home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+var Home = React.createClass({
+	render: function(){
+		return(
+			<div>
+				<h2>WeTech hopes to bridge the gender and diversity gap in the tech community by offering a mentorship program for women in technology. </h2>
+			</div>
+		)
+	}
+})
+
+export default Home;
