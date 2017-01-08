e474e5309662e0620220e6f5f9802199df3fe174
Ambar Gutierrez <ambar.grz@gmaail.com>
2017-01-07 20:21:42 -0500

    form for mentee

--- frontend/components/mentee.jsx
+++ frontend/components/mentee.jsx
@@ -7,11 +7,18 @@ var Mentee = React.createClass({
 			<div id='login'>
 				Register to get a mentor
 				<form>
-					Email
-					<input type='text'/>
-					Password
-					<input type='Password'/>
-					<input type='button'></input>
+					
+					<input type='text' placeholder='First Name'/>
+					<imput type='text' placeholder='Last Name' />
+					<imput type='email' placeholder='Email' />
+					<input type='Password' placeholder='Password'/>
+					<imput type='text' placeholder='Education' />
+					<input type='text' placeholder='Current Skills'/>
+					<p>Description - Why do you want to be a mentee? Who are you? What do you hope to do in the future?</p>
+					<input type='text' placeholder='Skills to Improve On/Gain'/>
+					<input type='url' placeholder='Do you have a Github?'/>
+					<input type='text' placeholder='Anything else we need to know about you'/>
+					<input type='button'>Create an account!</input>
 				</form>
 			</div>
 		)
