e536125af45a7a4dbcaff74b4474e9f667f5bf09
Vanessa Montoya <Vanessa@C4Qs-MacBook-Pro-4.local>
2017-01-08 04:46:15 -0500

    changes to style

--- frontend/components/login.jsx
+++ frontend/components/login.jsx
@@ -4,27 +4,22 @@ import {Link} from 'react-router'
 import '../loginCSS.css'
 
 var Login = React.createClass({
-
-	render(){
-		return(
-			<div id='login'>
-				<div className= "container">
-					<div className="wrapper">
-						<form name="Login_Form" className="form-signin">       
-						    <h3 className="form-signin-heading">Welcome Back! Please Sign In</h3>
-							  <hr className="colorgraph"/><br/>
-							  
-							  <input type="text" className="form-control" name="Username" placeholder="Username" required="" autoFocus="" />
-							  <input type="password" className="form-control" name="Password" placeholder="Password" required=""/>     		  
-					 		  <Link to="MenteeProf"><button className="button">Login</button></Link>
-						</form>
-						
-					</div>
+render(){
+	return(
+		<div id='login'>
+			<div className= "container">
+				<div className="wrapper">
+					<form name="Login_Form" className="form-signin">       
+					    <h3 className="form-signin-heading">Welcome Back! Please Sign In</h3>
+						  <input type="text" className="form-control" name="Username" placeholder="Username" />
+						  <input type="text" className="form-control" name="Password" placeholder="Password" />     		  
+				 		  <Link to="MenteeProf"><button className="button">Login</button></Link>
+					</form>
 				</div>
-			
 			</div>
-		)
-	}
+		</div>
+	)
+}
 })
 export default Login
 
